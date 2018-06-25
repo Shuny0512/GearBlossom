176e39aef109208eff7c87b7b8d40dfad914e114
Shuny0512 <shun_ny@icloud.com>
2018-06-25 22:13:09 +0900

    プレイヤー回転の作業継続中

--- GearBlossom/Assets/Stage3/Scripts/Stage3PlayerGenerator.cs
+++ GearBlossom/Assets/Stage3/Scripts/Stage3PlayerGenerator.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stage3PlayerGenerator : MonoBehaviour {
+public class Stage3PlayerGenerator : MonoBehaviour 
+{
 
 	public GameObject Stage3Player;
 	public GameObject Stage3PlayerObject1;
 	public GameObject Stage3PlayerObject2;
-	public int counter = 0;
+	public int counter = 1;
 	void Start () 
 	{
 		
@@ -15,36 +16,33 @@ public class Stage3PlayerGenerator : MonoBehaviour {
 	}
 	
 
+
 	void Update ()
+
 	{
+
 		if (Input.GetMouseButtonDown (0)) 
+	
 		{ 
+			
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
-			counter++;
 
+			Debug.Log (counter); 
 
-			if (counter == 2) 
+		
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) 
 			{
-				
-				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) 
+				if (hit.collider.CompareTag ("Player"))
+				{
+					this.gameObject.transform.Rotate (new Vector3 (0, 90, 0));
+				}
+
+				if (counter <= 2) 
 				{
 					Stage3PlayerObject1 = Instantiate (Stage3Player) as GameObject;
 					Stage3PlayerObject1.transform.position = new Vector3 (hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z);
-					Stage3PlayerObject1.transform.rotation = Quaternion.Euler (0, 90, 0);
-					Debug.Log (counter);
-
-				}
-			}
-
-			if (counter == 3) 
-			{
-				
-				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) 
-				{
-					Stage3PlayerObject2 = Instantiate (Stage3Player) as GameObject;
-					Stage3PlayerObject2.transform.position = new Vector3 (hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z);
-					Stage3PlayerObject2.transform.rotation = Quaternion.Euler (0, 180, 0);
+					counter++;
 					Debug.Log (counter);
 
 				}
